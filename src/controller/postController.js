2b2e6c4b7aec3cc8eab5c3ed0bec07fb2029025f
Duy <duyvu372001@gmail.com>
2024-04-01 15:15:15 +0900

    FollowUnFollow

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -1,14 +1,16 @@
 import User from "../models/userModel.js";
 import Post from "../models/postModels.js";
+import { v2 as cloudinary } from "cloudinary";
 
 const createPost = async (req, res) => {
   try {
-    const { postedBy, text, img } = req.body;
+    const { postedBy, text } = req.body;
+    let { img } = req.body;
 
     if (!postedBy || !text) {
       return res
         .status(400)
-        .json({ error: "Postedby and text fields are required" });
+        .json({ error: "Posted by and text fields are required" });
     }
 
     const user = await User.findById(postedBy);
@@ -25,6 +27,11 @@ const createPost = async (req, res) => {
       return res
         .status(400)
         .json({ error: `Text must be less than ${maxLength} characters` });
+    }
+
+    if (img) {
+      const uploadResponse = await cloudinary.uploader.upload(img);
+      img = uploadResponse.secure_url;
     }
 
     const newPost = new Post({ postedBy, text, img });
@@ -46,7 +53,7 @@ const getPost = async (req, res) => {
       return res.status(404).json({ error: "Post not found" });
     }
 
-    res.status(200).json({ post });
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in getPost: ", error.message);
@@ -62,7 +69,12 @@ const deletePost = async (req, res) => {
     }
 
     if (post.postedBy.toString() !== req.user._id.toString()) {
-      return res.status(404).json({ error: "Unauthorized to delete post" });
+      return res.status(401).json({ error: "Unauthorized to delete post" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
     }
 
     await Post.findByIdAndDelete(req.params.id);
@@ -81,7 +93,7 @@ const likeUnlikePost = async (req, res) => {
 
     const post = await Post.findById(postId);
     if (!post) {
-      return res.status(404).json({ message: "Post not found" });
+      return res.status(404).json({ error: "Post not found" });
     }
 
     const userLikedPost = post.likes.includes(userId);
@@ -110,12 +122,12 @@ const replyToPost = async (req, res) => {
     const username = req.user.username;
 
     if (!text) {
-      return res.status(404).json({ message: "Text field is required" });
+      return res.status(404).json({ error: "Text field is required" });
     }
 
     const post = await Post.findById(postId);
     if (!post) {
-      return res.status(404).json({ message: "Post not found" });
+      return res.status(404).json({ error: "Post not found" });
     }
 
     const reply = { userId, text, userProfilePic, username };
@@ -136,7 +148,7 @@ const getFeedPosts = async (req, res) => {
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
 
     const following = user.following;
@@ -145,10 +157,29 @@ const getFeedPosts = async (req, res) => {
       createAt: -1,
     });
 
-    res.status(200).json({ feedPosts });
+    res.status(200).json(feedPosts);
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in getFeedPosts: ", error.message);
+  }
+};
+
+const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const posts = await Post.find({ postedBy: user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in getUserPosts: ", error.message);
   }
 };
 
@@ -159,4 +190,5 @@ export {
   likeUnlikePost,
   replyToPost,
   getFeedPosts,
+  getUserPosts,
 };
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,13 +2,25 @@ import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
+  //query is either username or userID
+  const { query } = req.params;
   try {
-    const user = await User.findOne({ username })
-      .select("-password")
-      .select("-updatedAt");
+    let user;
+
+    //query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
     if (!user) return res.status(400).json({ error: "User not found" });
 
     res.status(200).json(user);
